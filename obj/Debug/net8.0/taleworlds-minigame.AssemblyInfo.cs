4adf9b8adb392da763048d3e98863fa219a12cdc
Bilal Dertli <127341202+bilalbdertli@users.noreply.github.com>
2024-06-12 11:19:06 +0300

    update gitignore

--- obj/Debug/net8.0/taleworlds-minigame.AssemblyInfo.cs
+++ obj/Debug/net8.0/taleworlds-minigame.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("taleworlds-minigame")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7c52f06a03b9d6a3baf21c0c25be4acbcce5fcb4")]
 [assembly: System.Reflection.AssemblyProductAttribute("taleworlds-minigame")]
 [assembly: System.Reflection.AssemblyTitleAttribute("taleworlds-minigame")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
